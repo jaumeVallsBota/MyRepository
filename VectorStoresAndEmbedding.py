c8e11224df65a49a9c0a6d44c0e02ec223c20521
jaumeVallsBota <150121531+jaumeVallsBota@users.noreply.github.com>
2023-11-28 11:35:55 +0100

    Create VectorStoresAndEmbedding.py

---
+++ VectorStoresAndEmbedding.py
@@ -0,0 +1,65 @@
+import os
+import openai
+import sys
+sys.path.append('../..')
+
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv()) # read local .env file
+
+openai.api_key  = os.environ['OPENAI_API_KEY']
+
+from langchain.document_loaders import PyPDFLoader
+
+# Load PDF
+loaders = [
+    # Duplicate documents on purpose - messy data
+    PyPDFLoader("docs/cs229_lectures/MachineLearning-Lecture01.pdf"),
+    PyPDFLoader("docs/cs229_lectures/MachineLearning-Lecture01.pdf"),
+    PyPDFLoader("docs/cs229_lectures/MachineLearning-Lecture02.pdf"),
+    PyPDFLoader("docs/cs229_lectures/MachineLearning-Lecture03.pdf")
+]
+docs = []
+for loader in loaders:
+    docs.extend(loader.load())
+
+
+# Split
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size = 1500,
+    chunk_overlap = 150
+)
+splits = text_splitter.split_documents(docs)
+from langchain.embeddings.openai import OpenAIEmbeddings
+embedding = OpenAIEmbeddings()
+
+sentence1 = "i like dogs"
+sentence2 = "i like canines"
+sentence3 = "the weather is ugly outside"
+
+embedding1 = embedding.embed_query(sentence1)
+embedding2 = embedding.embed_query(sentence2)
+embedding3 = embedding.embed_query(sentence3)
+
+import numpy as np
+
+np.dot(embedding1, embedding2)
+np.dot(embedding1, embedding3)
+np.dot(embedding2, embedding3)
+
+
+from langchain.vectorstores import Chroma
+persist_directory = 'docs/chroma/'
+#!rm -rf ./docs/chroma  # remove old database files if any
+
+vectordb = Chroma.from_documents(
+    documents=splits,
+    embedding=embedding,
+    persist_directory=persist_directory
+)
+print(vectordb._collection.count())
+
+question = "is there an email i can ask for help"
+docs = vectordb.similarity_search(question,k=3)
+
+vectordb.persist()
