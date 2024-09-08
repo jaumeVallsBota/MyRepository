80a3ababadd72ae749cee55b7420502226cb679c
Jaume <jaume.valls.26@gmail.com>
2024-09-08 16:34:00 +0200

    added structs and added comments in arrays.go

--- GO/arrays.go
+++ GO/arrays.go
@@ -4,24 +4,30 @@ import "fmt"
 
 func main() {
 
+	//definimos una array vacía de integers
     var a [5]int
     fmt.Println("emp:", a)
-
+	//se asigna una valor a la posición 4
     a[4] = 100
     fmt.Println("set:", a)
     fmt.Println("get:", a[4])
 
+	//comprobamos el tamaño de la array
     fmt.Println("len:", len(a))
 
-    b := [5]int{1, 2, 3, 4, 5}
+	//Le decimos el tamaño de la array y los elementos, en este caso los 5 primeros son los mostrados y los dos siguientes son 0
+    b := [7]int{1, 2, 3, 4, 5}
     fmt.Println("dcl:", b)
 
-    b = [...]int{1, 2, 3, 4, 5}
-    fmt.Println("dcl:", b)
+	//El compilador puede contar directamente el número de elementos de la array si ponemos [...], en este caso será 5
+    var c = [...]int{1, 2, 3, 4, 5}
+    fmt.Println("dcl:", c)
 
-    b = [...]int{100, 3: 400, 500}
-    fmt.Println("idx:", b)
-
+	//si especificas el indice (en este caso 3), los valores que hay en esta situación serán sustituidos por 0
+    var d = [...]int{100, 3: 400, 500}
+    fmt.Println("idx:", d)
+	
+	//crear una matriz de dos dimensiones por for
     var twoD [2][3]int
     for i := 0; i < 2; i++ {
         for j := 0; j < 3; j++ {
@@ -29,7 +35,7 @@ func main() {
         }
     }
     fmt.Println("2d: ", twoD)
-
+	//definir una matriz de dos dimensiones directamente
     twoD = [2][3]int{
         {1, 2, 3},
         {1, 2, 3},
